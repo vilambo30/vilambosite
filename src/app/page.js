56c8edf479457c49331e80a8f5c86cf4276d2c3b
vilambo30 <vilamboprivatelimited@gmail.com>
2024-09-13 16:45:15 +0530

    changed page,jsc

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,4 @@
 import AboutUs from "./aboutus/about";
-import ContactForm from "./contact/contactForm";
-import Footer from "./footer/footer";
-import Navbar from "./header/navbar";
 import HeroSection from "@/app/hero/hero";
 import Services from "./services/services";
 
